9e10c2a34b9fdf92350a2d00b03a6a2a2a50e3d0
Andrew Ooi <andrew20010924@hotmail.com>
2022-04-25 20:39:51 +0800

    Developed AttackBehaviour class and added 3 new classes for REQ3

--- src/game/AttackBehaviour.java
+++ src/game/AttackBehaviour.java
@@ -2,13 +2,51 @@ package game;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
 
+import java.util.Random;
+
+/**
+ * A class that dictates how an enemy attacks the Player
+ */
 public class AttackBehaviour implements Behaviour{
 
-    // TODO: develop and use it to attack the player automatically.
+    /**
+     * The Actor that is to be attacked
+     */
+    private Actor target;
+
+    /**
+     * Random object used to generate probabilities
+     */
+    private Random random = new Random();
+
+    /**
+     * Constructor
+     * @param target The actor that is to be attacked
+     */
+    public AttackBehaviour(Actor target){
+        this.target = target;
+    }
+
     @Override
     public Action getAction(Actor actor, GameMap map) {
+        Location here = map.locationOf(actor);
+        Location there = map.locationOf(target);
+
+        for (Exit exit : here.getExits()){
+            Location destination = exit.getDestination();
+            if (destination == there){
+                if(actor instanceof Goomba) {
+                    return new KickAction(target, exit.getName());
+                }
+                else if(actor instanceof Koopa){
+                    return new PunchAction(target, exit.getName());
+                }
+            }
+        }
         return null;
     }
 }
