ebe7ce7f40c56c8769071b4c566bb89be1cc4d5c
nrav0005 <nrav0005@student.monash.edu>
2022-05-20 15:30:07 +0800

    Bows now use and consume up Arrows.

--- src/game/items/Arrow.java
+++ src/game/items/Arrow.java
@@ -3,11 +3,16 @@ package game.items;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.DropItemAction;
 import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.items.PickUpItemAction;
+import edu.monash.fit2099.engine.positions.Location;
+import game.GameUtilities;
+import game.Status;
+import game.actions.PickUpStackableAction;
 
 /**
  * A stick with a sharp metal tip used for killing
  */
-public class Arrow extends Item implements Tradable {
+public class Arrow extends Item implements Tradable, Stackable {
 
     /**
      * The price of an arrow
@@ -23,21 +28,18 @@ public class Arrow extends Item implements Tradable {
      * Constructor
      */
     public Arrow(int arrowCount){
-        super("Arrow", 'A', true);
+        super("Arrow", 'A', false);
         this.arrowCount = arrowCount;
     }
 
     /**
-     * Returns the number of arrows
-     * @return the number of arrows
+     * Returns a new instance of PickUpStorableAction
+     * @param actor - the actor picking up this coin
+     * @return a PickUpStorable instance to pick up this instance
      */
-    public int getArrowCount(){
-        return arrowCount;
-    }
-
     @Override
-    public DropItemAction getDropAction(Actor actor){
-        return null;
+    public PickUpItemAction getPickUpAction(Actor actor) {
+        return new PickUpStackableAction(this, this);
     }
 
     /**
@@ -56,5 +58,24 @@ public class Arrow extends Item implements Tradable {
     @Override
     public Item getItem() {
         return this;
+    }
+
+    /**
+     * Returns the number of arrows
+     * @return the number of arrows
+     */
+    @Override
+    public int getAmount() {
+        return this.arrowCount;
+    }
+
+    @Override
+    public Storable getStorableType() {
+        return Storable.ARROW;
+    }
+    public void tick(Location currentLocation, Actor actor) {
+        MagicPouch magicPouch = (MagicPouch) GameUtilities.getItemWithCapability(actor, Status.CAN_CARRY_STORABLES);
+        magicPouch.increaseAmount(Storable.ARROW, this.getAmount());
+        actor.removeItemFromInventory(this);
     }
 }
