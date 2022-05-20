9e57f23387ae6527fa0503e4e692b414bc618327
nrav0005 <nrav0005@student.monash.edu>
2022-05-20 14:13:45 +0800

    The Terminator finally works

--- src/game/behaviours/ChaseBehaviour.java
+++ src/game/behaviours/ChaseBehaviour.java
@@ -65,25 +65,41 @@ public class ChaseBehaviour implements Behaviour {
 
       ArrayList<Location> visited = new ArrayList<Location>();
       ArrayList<Location> unvisited = new ArrayList<Location>();
-
+      HashMap<Location, Location> parents = new HashMap<>();
+      parents.put(here, null);
       visited.add(here);
-      Exit intendedExit = null;
-      for (Exit exit: here.getExits()) {
-         while (!notVisited.isEmpty()) {
-            Location v = this.popNearest();
-            if (v.containsAnActor() && v.getActor() == this.target) {
-               intendedExit = exit;
-               break;
-            }
-            else {
+      unvisited.add(here);
+      while (!unvisited.isEmpty()) {
+         Location v = unvisited.get(0);
+         unvisited.remove(0);
 
-            }
-         }
-         if (intendedExit != null) {
+         if (v.containsAnActor() && v.getActor() == this.target) {
             break;
          }
+
+         for (Exit w: v.getExits()) {
+            Location destination = w.getDestination();
+            if (!visited.contains(destination) && (destination.canActorEnter(actor) || (destination.containsAnActor() && destination.getActor() == this.target))) {
+               parents.put(destination, v);
+               visited.add(destination);
+               unvisited.add(destination);
+            }
+         }
       }
-      //return new MoveActorAction(intendedExit.getDestination(), intendedExit.getName());
+
+      if (parents.get(there) == null) {
+         return null;
+      }
+      Location parent = there;
+      while (parents.get(parent) != here) {
+         parent = parents.get(parent);
+      }
+      for (Exit exit: here.getExits()) {
+         if (exit.getDestination() == parent) {
+            return new MoveActorAction(exit.getDestination(), exit.getName());
+         }
+      }
+
       return null;
    }
 }
